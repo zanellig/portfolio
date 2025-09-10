4512661d662b9ce77b2e50860e3619caaed90dcf
zanellig <gonzalozanelli1@gmail.com>
2025-09-10 17:43:34 -0300

    fix(dto)!: implement correct schema for blog posts

--- apps/server/src/db/dto/blog.ts
+++ apps/server/src/db/dto/blog.ts
@@ -1,17 +1,28 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import z from "zod";
-import { postFormat, posts, postStatus } from "../schema/blog";
+import { posts } from "../schema/blog";
 
-export const insertPostSchema = createInsertSchema(posts, {
+const _insertPostSchema = createInsertSchema(posts, {
   slug: z.string().regex(/^[a-z0-9-]+$/),
   title: z.string().min(1).max(128),
   excerpt: z.string().max(255).optional(),
   body: z.string().min(1),
-  status: z.enum(postStatus._.enumValues).optional(),
-  format: z.enum(postFormat._.enumValues).optional(),
   isFeatured: z.boolean().optional(),
+  isCommentable: z.boolean().optional(),
+});
+export const selectPostSchema = createSelectSchema(posts);
+
+export const insertPostSchema = _insertPostSchema.pick({
+  title: true,
+  body: true,
+  coverImageId: true,
+  excerpt: true,
+  format: true,
+  isCommentable: true,
+  isFeatured: true,
+  meta: true,
+  slug: true,
 });
 
-export const selectPostSchema = createSelectSchema(posts);
 export type InsertPost = z.infer<typeof insertPostSchema>;
 export type SelectPost = z.infer<typeof selectPostSchema>;
