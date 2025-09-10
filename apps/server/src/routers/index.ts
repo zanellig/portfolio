38a7eb0bba36d6e99d6e8cda1cf449cdf8012a3f
zanellig <gonzalozanelli1@gmail.com>
2025-09-10 17:44:26 -0300

    feat(router): implement post creation endpoint & mock image upload

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -3,34 +3,65 @@ import { protectedProcedure, publicProcedure, router } from "../lib/trpc";
 import { db } from "@/db";
 import z from "zod";
 import { desc, eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { insertPostSchema, selectPostSchema } from "@/db/dto/blog";
 
 export const appRouter = router({
   healthCheck: publicProcedure.query(() => {
     return "OK";
   }),
+
   privateData: protectedProcedure.query(({ ctx }) => {
     return {
       message: "This is private",
       user: ctx.session.user,
     };
   }),
+
   posts: publicProcedure.query(async () => {
-    const postsRes = await db
-      .select()
-      .from(posts)
-      .orderBy(desc(posts.createdAt));
-    if (!postsRes) return [];
-    return postsRes;
+    try {
+      const postsRes = await db
+        .select()
+        .from(posts)
+        .orderBy(desc(posts.createdAt));
+
+      return z.array(selectPostSchema).parse(postsRes);
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
   }),
+
   postById: publicProcedure.input(z.uuid()).query(async (opts) => {
     return await db.select().from(posts).where(eq(posts.id, opts.input));
   }),
-  postCreate: protectedProcedure
-    .input(z.object({ title: z.string().max(255) }))
+
+  uploadImage: protectedProcedure
+    .input(z.instanceof(FormData))
     .mutation(async (opts) => {
-      const { title } = opts.input;
-      const post = await db.insert(posts).values({ title }).$returningId();
-      return post;
+      const file = opts.input.get("file");
+      if (file === null || !file || file === "null")
+        throw new TRPCError({
+          message: "A file must be included",
+          code: "BAD_REQUEST",
+        });
+      return z.uuid().parse("1ee6dc69-b577-4872-aed6-a88b4778e706");
+    }),
+
+  postCreate: protectedProcedure
+    .input(insertPostSchema)
+    .mutation(async (opts) => {
+      const post = opts.input;
+      try {
+        const postRes = await db.insert(posts).values(post).$returningId();
+        return postRes;
+      } catch (e: any) {
+        console.error(e);
+        throw new TRPCError({
+          message: e.cause,
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
     }),
 });
 export type AppRouter = typeof appRouter;
