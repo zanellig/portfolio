25cdcf4b08c46a7a65f0199a26543a3e16c6e39a
zanellig <gonzalozanelli1@gmail.com>
2025-09-10 17:41:58 -0300

    feat(auth)!: add jwt support

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,22 +1,31 @@
 import { betterAuth } from "better-auth";
+import { jwt } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "mysql",
-		schema: schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: "none",
-			secure: true,
-			httpOnly: true,
-		},
-	},
+  database: drizzleAdapter(db, {
+    provider: "mysql",
+    schema: schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ""],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [jwt()],
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+    cookiePrefix: "pf",
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
 });
