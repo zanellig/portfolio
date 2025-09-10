150ad62bc1df6f570dca2d1d40b485fa6d17419e
zanellig <gonzalozanelli1@gmail.com>
2025-09-10 01:55:30 -0300

    even simpler (wow)

--- apps/web/src/utils/server-trpc.ts
+++ apps/web/src/utils/server-trpc.ts
@@ -4,22 +4,10 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 
-import {
-  createTRPCContext,
-  createTRPCOptionsProxy,
-} from "@trpc/tanstack-react-query";
+import { createTRPCContext } from "@trpc/tanstack-react-query";
 
-import {
-  createTRPCClient,
-  httpBatchLink,
-  httpLink,
-  isNonJsonSerializable,
-  splitLink,
-} from "@trpc/client";
-
-import { appRouter, type AppRouter } from "../../../server/src/routers";
+import { appRouter } from "../../../server/src/routers";
 import { cache } from "react";
-import { t } from "../../../server/src/lib/trpc";
 
 export function makeQueryClient() {
   return new QueryClient({
@@ -40,38 +28,5 @@ export function makeQueryClient() {
   });
 }
 
-const trpcClient = createTRPCClient<AppRouter>({
-  links: [
-    splitLink({
-      condition: (op) => isNonJsonSerializable(op.input),
-      true: httpLink({
-        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/trpc`,
-        fetch(url, options) {
-          return fetch(url, {
-            ...options,
-            credentials: "include",
-          });
-        },
-      }),
-      false: httpBatchLink({
-        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/trpc`,
-        fetch(url, options) {
-          return fetch(url, {
-            ...options,
-            credentials: "include",
-          });
-        },
-      }),
-    }),
-  ],
-});
-
 export const getQueryClient = cache(makeQueryClient);
 export const caller = appRouter.createCaller(createTRPCContext);
-
-export const trpc = createTRPCOptionsProxy({
-  client: trpcClient,
-  ctx: createTRPCContext,
-  router: appRouter,
-  queryClient: getQueryClient,
-});
